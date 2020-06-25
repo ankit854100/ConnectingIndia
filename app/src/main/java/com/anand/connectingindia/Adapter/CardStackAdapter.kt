b0338e6e6a1525c9363be5e348042bc082881d9a
ankit <ankit854100@gmail.com>
2020-06-25 17:29:49 +0530

    card view implemented

---
+++ app/src/main/java/com/anand/connectingindia/Adapter/CardStackAdapter.kt
@@ -0,0 +1,53 @@
+package com.anand.connectingindia.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.anand.connectingindia.Model.Spot
+import com.anand.connectingindia.R
+import com.bumptech.glide.Glide
+
+class CardStackAdapter(
+    private var spots: List<Spot> = emptyList()
+) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(inflater.inflate(R.layout.item_card, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val spot = spots[position]
+        holder.name.text = "${spot.id}. ${spot.name}"
+        holder.city.text = spot.city
+        Glide.with(holder.image)
+            .load(spot.url)
+            .into(holder.image)
+        holder.itemView.setOnClickListener { v ->
+            Toast.makeText(v.context, spot.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return spots.size
+    }
+
+    fun setSpots(spots: List<Spot>) {
+        this.spots = spots
+    }
+
+    fun getSpots(): List<Spot> {
+        return spots
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.item_name)
+        var city: TextView = view.findViewById(R.id.item_city)
+        var image: ImageView = view.findViewById(R.id.item_image)
+    }
+
+}
--- app/src/main/java/com/anand/connectingindia/HomeActivity.kt
+++ app/src/main/java/com/anand/connectingindia/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.anand.connectingindia.Fragments.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_home.*
@@ -28,5 +30,38 @@ class HomeActivity : AppCompatActivity() {
 
         }
 
+        var homeFragment = HomeFragment()
+        var discoverFragment = DiscoverFragment()
+        var postFragment = PostFragment()
+        var communityFragment = CommunityFragment()
+        var notificationFragment = NotificationFragment()
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> makeCurrentFragment(homeFragment)
+                R.id.nav_discover -> makeCurrentFragment(discoverFragment)
+                R.id.nav_post -> makeCurrentFragment(postFragment)
+                R.id.nav_communities -> makeCurrentFragment(communityFragment)
+                R.id.nav_notification -> makeCurrentFragment(notificationFragment)
+
+            }
+
+            true
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, homeFragment)
+            commit()
+        }
+
     }
+
+
+    private fun makeCurrentFragment(Fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, Fragment)
+            addToBackStack(null)
+            commit()
+        }
+
 }
