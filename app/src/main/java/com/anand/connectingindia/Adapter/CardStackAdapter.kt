51ee9372cf27552f5002285d34e506fc1a881c66
ankit <ankit854100@gmail.com>
2020-07-02 17:02:44 +0530

    Initial version of video post(having problem of play in background) in HomeFragment

--- app/src/main/java/com/anand/connectingindia/Adapter/CardStackAdapter.kt
+++ app/src/main/java/com/anand/connectingindia/Adapter/CardStackAdapter.kt
@@ -15,16 +15,31 @@ import com.anand.connectingindia.Model.Spot
 import com.anand.connectingindia.R
 import com.borjabravo.readmoretextview.ReadMoreTextView
 import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 
 class CardStackAdapter(
     private var spots: List<Spot> = emptyList()
 ) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
 
     private lateinit var mContext: Context
+    var bandwidthMeter  = DefaultBandwidthMeter()
+    var trackSelector = DefaultTrackSelector(AdaptiveTrackSelection.Factory(bandwidthMeter))
+    private lateinit var simpleExoPlayer : SimpleExoPlayer
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
+
         mContext = parent.context
+        simpleExoPlayer =ExoPlayerFactory.newSimpleInstance(mContext, trackSelector)
+
         return ViewHolder(inflater.inflate(R.layout.item_card, parent, false))
     }
 
@@ -43,6 +58,8 @@ class CardStackAdapter(
         if (spot.url.isNotEmpty()){
 
             holder.textPostLayout.visibility = View.GONE
+            holder.simpleExoPLayerView.visibility = View.GONE
+            simpleExoPlayer.release()
             holder.image.visibility = View.VISIBLE
 
             holder.name.text = "${spot.id}. ${spot.name}"
@@ -52,16 +69,34 @@ class CardStackAdapter(
             .into(holder.image)
 
         }
-//        if (spot.video.isNotEmpty()){
-//            holder.videoView.visibility = View.VISIBLE
-//            holder.videoView.setVideoURI(Uri.parse(spot.video))
-//            holder.videoView.start()
-//        }
+
+        if (spot.video.isNotEmpty()){
+
+            holder.image.visibility = View.GONE
+            holder.textPostLayout.visibility = View.GONE
+            holder.simpleExoPLayerView.visibility = View.VISIBLE
+
+
+//            var bandwidthMeter  = DefaultBandwidthMeter()
+//            var trackSelector = DefaultTrackSelector(AdaptiveTrackSelection.Factory(bandwidthMeter))
+//            simpleExoPlayer =ExoPlayerFactory.newSimpleInstance(mContext, trackSelector)
+            var uri = Uri.parse(spot.video)
+            var dataSourceFactory = DefaultHttpDataSourceFactory("exoplayer_video")
+            var extractorFactory = DefaultExtractorsFactory()
+            var mediaSource = ExtractorMediaSource(uri, dataSourceFactory, extractorFactory, null, null)
+            holder.simpleExoPLayerView.player = simpleExoPlayer
+            simpleExoPlayer.prepare(mediaSource)
+            simpleExoPlayer.playWhenReady = true
+
+        }
+
         if (spot.postText.isNotEmpty()){
 
             holder.scrollView.scrollTo(0,0)
 
             holder.image.visibility = View.GONE
+            holder.simpleExoPLayerView.visibility = View.GONE
+            simpleExoPlayer.release()
             holder.textPostLayout.visibility = View.VISIBLE
 
             holder.name.text = "${spot.id}. ${spot.name}"
@@ -117,6 +152,8 @@ class CardStackAdapter(
         var scrollView : NestedScrollView = view.findViewById(R.id.scrollView)
         var readMoreText : ReadMoreTextView = view.findViewById(R.id.read_more_text)
         var textPostLayout : LinearLayout = view.findViewById(R.id.text_post_layout)
+//        var videoContainer : RelativeLayout = view.findViewById(R.id.video_container)
+        var simpleExoPLayerView : SimpleExoPlayerView = view.findViewById(R.id.simpleExoplayerView)
     }
 
 }
